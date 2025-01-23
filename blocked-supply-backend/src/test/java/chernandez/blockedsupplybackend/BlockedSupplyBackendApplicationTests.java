00f7093960d556458ae2ae21f308ca18335bff50
Carles Hernandez <chernandezf@edu.tecnocampus.cat>
2025-01-23 18:46:24 +0100

    project init

---
+++ blocked-supply-backend/src/test/java/chernandez/blockedsupplybackend/BlockedSupplyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package chernandez.blockedsupplybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlockedSupplyBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
