a9ca16e56a6d65776ae38865ca5daa902ddc5e75
Carles Hernandez <chernandezf@edu.tecnocampus.cat>
2025-02-08 12:33:12 +0100

    CU-8697t582e Create Transaction API

--- blocked-supply-backend/src/main/java/chernandez/blockedsupplybackend/domain/ShipmentInput.java
+++ blocked-supply-backend/src/main/java/chernandez/blockedsupplybackend/domain/ShipmentInput.java
@@ -3,7 +3,7 @@ package chernandez.blockedsupplybackend.domain;
 import lombok.Data;
 
 @Data
-public class ShipmentDTO {
+public class ShipmentInput {
     private String name;
     private String description;
     private String origin;
@@ -11,10 +11,10 @@ public class ShipmentDTO {
     private int units;
     private int weight;
 
-    public ShipmentDTO() {
+    public ShipmentInput() {
     }
 
-    public ShipmentDTO(String name, String description, String origin, String destination, int units, int weight) {
+    public ShipmentInput(String name, String description, String origin, String destination, int units, int weight) {
         this.name = name;
         this.description = description;
         this.origin = origin;
